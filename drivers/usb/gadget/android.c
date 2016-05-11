1e6d6f222f21edcccf6f1ff6f973926f2eb68813
Tony Malagisi <tvm2487@gmail.com>
2016-05-11 09:05:33 -0400

    Revert "HID keyboard/mouse gadget support"
    This reverts commit 3ccee705c57294b5c82d2aca58134878281cb491.

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -43,9 +43,6 @@
 #define USB_ETH_RNDIS y
 #include "f_rndis.c"
 #include "rndis.c"
-#include "f_hid.h"
-#include "f_hid_android_keyboard.c"
-#include "f_hid_android_mouse.c"
 
 #include <plat/cpu.h>
 
@@ -1427,42 +1424,6 @@ static struct android_usb_function midi_function = {
 	.attributes	= midi_function_attributes,
 };
 
-static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
-{
-	return ghid_setup(cdev->gadget, 2);
-}
-
-static void hid_function_cleanup(struct android_usb_function *f)
-{
-	ghid_cleanup();
-}
-
-static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
-{
-	int ret;
-	printk(KERN_INFO "hid keyboard\n");
-	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
-	if (ret) {
-		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
-		return ret;
-	}
-	printk(KERN_INFO "hid mouse\n");
-	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
-	if (ret) {
-		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
-		return ret;
-	}
-	return 0;
-}
-
-static struct android_usb_function hid_function = {
-	.name		= "hid",
-	.init		= hid_function_init,
-	.cleanup	= hid_function_cleanup,
-	.bind_config	= hid_function_bind_config,
-};
-#endif
-
 static struct android_usb_function *supported_functions[] = {
 	&adb_function,
 	&acm_function,
@@ -1484,7 +1445,6 @@ static struct android_usb_function *supported_functions[] = {
 	&qacm_function,
 #endif
 	&midi_function,
-	&hid_function,
 	NULL
 };
 
@@ -1684,11 +1644,6 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 
 #endif
 	}
-
-	/* Always enable HID gadget function. */
-	err = android_enable_function(dev, "hid");
-	if (err)
-		pr_err("android_usb: Cannot enable hid (%d)", err);
 
 	mutex_unlock(&dev->mutex);
 
